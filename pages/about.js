96670067da6e3954b47279f2cc867c2daad3d678
DESKTOP-3E7M56Q <as899533@gmail.com>
2023-03-22 22:17:45 +0530

    initial

---
+++ pages/about.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
+          mollitia aliquam adipisci eligendi cumque minima facilis nobis debitis
+          error beatae!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
+          mollitia aliquam adipisci eligendi cumque minima facilis nobis debitis
+          error beatae!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
